a26475299ffd46410c68c29e03011d0440947d24
KATARI REVATHI <katari.revathi@gmail.com>
2024-11-19 16:12:30 +0530

    utils

--- transaction/urls.py
+++ transaction/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenVerifyView, TokenRefreshView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('transactions_app.urls')),
+    path('api/token/', TokenObtainPairView.as_view()),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view()),
 ]
+handler404 = 'utils.error_views.handler404'
+handler500 = 'utils.error_views.handler500'
